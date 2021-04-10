59ae31ca6d2fde5bdfbac687a2237d8b92aab50f
Jayden Lee <34024244+Shroomy1@users.noreply.github.com>
2021-04-10 11:58:10 +1000

    functions
    finished data scraping and functions to interpret data. Data is now put into data.csv in workable way.

--- htmlPulling.py
+++ htmlPulling.py
@@ -23,6 +23,7 @@ def rain():
     results = soup.find_all("em", class_='pop')
     results_string = str(results)
     found = re.findall('\d+(?=\%)', results_string)
+    found_string = str(found)
         
     #return results_string
     return found
@@ -35,18 +36,29 @@ def summary():
 
     results = soup.find_all("dd", class_='summary')
     results_string = str(results)
-    return results_string
+    found = re.findall('(?<=\>)(.*?)(?=\<)', results_string)
 
-print(rain())
+    return found
+
+def day_otw():
+    URL = 'http://www.bom.gov.au/act/forecasts/canberra.shtml'
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    results = soup.find_all("h2")
+    results_string = str(results)
+    found = re.findall('(?<=\>)(.*?)(?=\<)', results_string)
+    return found
 
 with open('data.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-    writer.writerow(["temp", "date", "rain" "weather"])
-    writer.writerow([Max_Numbers()[17:19], "Friday", "Partly Cloudy"])
-    writer.writerow([Max_Numbers()[42:44], "Saturday"])
-    writer.writerow([Max_Numbers()[67:69], "Saturday"])
-    writer.writerow([Max_Numbers()[92:94], "Saturday"])
-    writer.writerow([Max_Numbers()[117:119], "Saturday"])
-    writer.writerow([Max_Numbers()[142:144], "Saturday"])
-    writer.writerow([Max_Numbers()[167:169], "Saturday"])
+    writer.writerow(["temp", "date", "rain", "weather"])
+    writer.writerow([Max_Numbers()[17:19], day_otw()[0], rain()[0] ,summary()[0]])
+    writer.writerow([Max_Numbers()[42:44], day_otw()[2], rain()[1], summary()[2]])
+    writer.writerow([Max_Numbers()[67:69], day_otw()[4], rain()[2], summary()[4]])
+    writer.writerow([Max_Numbers()[92:94], day_otw()[6], rain()[3], summary()[6]])
+    writer.writerow([Max_Numbers()[117:119], day_otw()[8], rain()[4], summary()[8]])
+    writer.writerow([Max_Numbers()[142:144], day_otw()[10], rain()[5], summary()[10]])
+    writer.writerow([Max_Numbers()[167:169], day_otw()[12], rain()[6], summary()[12]]) 
     
